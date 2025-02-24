ea67d5ab157528801d730933610abffa35d7c3fa
Traur111 <leojordan.111@gmail.com>
2025-02-24 18:03:23 +0100

    Creation des fichiers

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+include_once("header.php");
+?>
+
+    <br>
+    <div class="row">
+
+    <div class="col-lg-4 col-md-6">
+        <div class="card">
+            <!-- <img src= "./assets/images/"> -->
+                <div class="card-body">
+                <h5 class="card-title"> titre </h5>
+                <p class="card-text"> resume </p>
+                <a href="./single.php" class="btn btn-primary" target="_blank">Article</a>
+            </div>
+        </div>
+    </div>
+
+</div>
+ 
+    <br>
+    <a href="./contact.php" target="_blank">Contact</a>
+
+<?php
+include_once("footer.php");
+?>
---
+++ single.php
@@ -0,0 +1,22 @@
+
+<?php
+include_once("header.php");
+?>
+
+<div class="row">
+    <div class="col-lg-4 col-md-6">
+        <div class="card">
+            <!-- <img src="./assets/images/" class="card-img-top" alt="..."> -->
+                <div class="card-body">
+                <h5 class="card-title"> Titre</h5>
+                    <p class="card-text"> Content</p>
+                </div>
+        </div>
+    </div>
+</div>
+
+<a href="./index.php" target="_blank">Retour page d'acceuil</a>
+
+<?php
+include_once("footer.php");
+?>
